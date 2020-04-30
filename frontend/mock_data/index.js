f9843e18f57d504f5a1c83823b67c76df92b5bd9
Sam An <smpmemento@gmail.com>
2020-04-29 18:09:17 -0700

    style changes

--- frontend/mock_data/index.js
+++ frontend/mock_data/index.js
@@ -2,26 +2,31 @@ export default [
 	{
 		userName: 'james',
 		title: 'my recent experience with Starbucks',
+		review: ' * * ',
 		data: '',
 	},
 	{
 		userName: 'sam',
 		title: 'Do not go there.',
+		review: ' * ',
 		data: '',
 	},
 	{
 		userName: 'will',
 		title: 'Things you should avoid when you go to Coffeebean',
+		review: ' * * *',
 		data: '',
 	},
 	{
 		userName: 'jun',
 		title: 'How to enjoy your life with a cup of coffee',
+		review: ' * * * * *',
 		data: '',
 	},
 	{
 		userName: 'ryan',
 		title: 'Recently I started coffee at night and then...',
+		review: ' * * * *',
 		data: '',
 	},
 ];
--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,17 +1,77 @@
 import React, { Fragment } from 'react';
-import './style.css';
+import styled from 'styled-components';
+
+
+const NavBar = styled.nav`
+	display: flex;
+	flex-flow: row nowrap;
+	justify-contents: space-evenly;
+	align-items: center;
+
+	li {
+		display: inline;
+		float: left;
+	}
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 15;
+		overflow: hidden;
+		background-color: #ecebdd;
+		position:sticky;
+	}
+	li a {
+		display: block;
+		color: #000000;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+	}
+	
+	li a:hover:not(.active) {
+		border: 2px solid #383287;
+	}
+	.logo{
+		font-size:35px;
+		font-weight: bold;
+		color: #383287;
+		float: left;
+		padding: 10px;
+	}
+	.searchBar{
+		float:right;
+		padding: 10px;
+	}
+`
+
+const tempStyles = {
+	searchStyle: {
+		width: '300px',
+		border: '2px solid #383287',
+		height: '40px',
+	}
+};
 const Header = () => {
 	return (
 		<Fragment>
-			<header className = "header">
-			<nav className = "headerMenu">
-				<a href ="#">bean there</a>
-				<a href ="#">bean good</a>
-				<a href ="#">have you bean?</a>
-				<a href ="#">about</a>
-				<a href ="#">log in</a>
-			</nav>
-			</header>
+			<NavBar>
+			    <div className = "logo">bean there</div>
+				
+				<ul>
+				<li><a href ="bean good">bean good</a></li>
+				<li><a href ="hyb">have you bean?</a></li>
+				<li><a href ="about">about</a></li>
+				<li><a href ="log in">log in</a></li>
+				</ul>
+
+				<div className = "searchBar">
+				<input
+					type='search'
+					placeholder='search'
+					style={tempStyles.searchStyle}
+				></input>
+			</div>
+			</NavBar>
 			<hr />
 		</Fragment>
 	);
